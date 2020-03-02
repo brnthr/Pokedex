ddd60d791664bdaead4d05f99d07c08de318dba1
Bryan Theurier <bryan.theurier.pro@gmail.com>
2020-03-02 23:04:35 +0100

    premier rendu

--- pokedex/routes/pokemons.js
+++ pokedex/routes/pokemons.js
@@ -1,5 +1,11 @@
 var router = require('express').Router();
 
+var Pokemon = require('./../models/Pokemon');
 
+router.get('/',(req, res) => {
+    Pokemon.find({}).populate('types').then(pokemons => {
+      res.render('pokemons/index.html', {pokemons: pokemons});
+    });
+});
 
 module.exports = router;
