dda7967354b688901a7e1b822935891adb64ace6
Bryan Theurier <bryan.theurier.pro@gmail.com>
2020-03-21 22:24:28 +0100

    Affichage de /edit & /new

--- pokedex/routes/pokemons.js
+++ pokedex/routes/pokemons.js
@@ -1,11 +1,12 @@
 var router = require('express').Router();
 
 var Pokemon = require('./../models/Pokemon');
+var Type = require('./../models/Type');
 
 router.get('/',(req, res) => {
     Pokemon.find().populate('types')
     .then(pokemons => {
-      console.log(pokemons)
+      //console.log(pokemons);
       res.render('./../views/pokemons/index.html', { pokemons : pokemons });
     })
     .catch(err => {
@@ -13,12 +14,38 @@ router.get('/',(req, res) => {
     });
 });
 
+router.get('/new', (req, res) => {
+  Type.find({})
+  .then(types => {
+    const pokemon = new Pokemon();
+    res.render('./../views/pokemons/edit.html', { pokemon : pokemon, types : types });
+  })
+  .catch(err => {
+    console.error(err);
+  });
+});
+
+router.get('/edit/:id', (req, res) => {
+  Type.find({})
+  .then(types => {
+    Pokemon.findById(req.params.id).then(pokemon => {
+      //console.log(pokemon);
+      res.render('./../views/pokemons/edit.html', { pokemon : pokemon, types : types });
+    });
+  })
+  .catch(err => {
+    console.error(err);
+  });
+});
+
+
 router.get('/:id', (req, res) => {
   Pokemon.findById(req.params.id).populate('types')
   .then(pokemon => {
-    console.log(pokemon);
-    res.render('./../views/pokemons/show.html', { pokemon : pokemon });
-  }).catch(err => {
+    //console.log(pokemon);
+    //res.render('./../views/pokemons/show.html', { pokemon : pokemon });
+  })
+  .catch(err => {
     res.status(404);
     res.send("404");
   });
